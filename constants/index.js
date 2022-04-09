d222e94439110b238708564aac6723a39d224491
DumasOlivier <dumas.olivier@outlook.fr>
2022-04-09 15:03:01 +0200

    Update METADATA_URL

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 //  https://github.com/DumasOlivier/Whitelist-Dapp/tree/master/contracts
 const WHITELIST_CONTRACT_ADDRESS = '0x7CdF7Fd2BbA9CAe4037a5BE1b8fC3ebe0dD66aA5';
-const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+const METADATA_URL = 'https://nft-collection-front-seven.vercel.app/api/';
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
